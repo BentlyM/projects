2181d688ab6ec949a3050dca788483c08cfed58f
bentlym <metayerbently@gmail.com>
2024-05-16 00:25:29 -0400

    day 2 added

---
+++ adventOfCode/day-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream> 
+using namespace std;
+
+int main(){
+    // Advent of Code
+    // Day 2: I was Told there would be no math
+    string input = "";
+    int l = 0;
+    int w = 0;
+    int h = 0;
+    const int literal = 2;
+    fstream infile("input.txt", ios::in);
+}
