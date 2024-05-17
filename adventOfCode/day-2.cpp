51b1420066f37473cfa7b12b857924d2095981c8
bentlym <metayerbently@gmail.com>
2024-05-17 13:54:15 -0400

    day 2

--- adventOfCode/day-2.cpp
+++ adventOfCode/day-2.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream> 
+#include <algorithm>
 using namespace std;
 
 int main(){
@@ -13,4 +14,21 @@ int main(){
     int h = 0;
     const int literal = 2;
     fstream infile("input.txt", ios::in);
+    while(infile){
+        getline(infile, input);
+        if(input.find("x") != string::npos){
+            l = stoi(input.substr(0, input.find("x")));
+            w = stoi(input.substr(input.find("x")+1, input.find("x")+1));
+            h = stoi(input.substr(input.find("x")+1, input.find("x")+1));
+        }
+    }
+    int area = 2*l*w + 2*w*h + 2*h*l;
+            int bow = l*w*h;
+            int ribbon = 2*l + 2*w + l*w*h;
+            cout << "The area of the wrapping paper is " << area << " square feet." << endl;
+            cout << "The area of the ribbon is " << ribbon << " feet." << endl;
+            cout << "The bow is " << bow << " feet." << endl;
+            cout << "The total area of the wrapping paper and ribbon is " << area + ribbon << " feet." << endl;
+            cout << "The total area of the wrapping paper, ribbon, and bow is " << area + ribbon + bow << " feet." << endl
+            << endl;
 }
