44417f8a8161319c3fb0f0d052a72ff4e02475ec
bentlym <metayerbently@gmail.com>
2024-05-25 11:59:01 -0400

    test

--- exercise.js
+++ exercise.js
@@ -1,13 +1,5 @@
-
-function capitalize(s , num){
-  let randomIndex = Math.floor(Math.random() * s.length);
-  if(num === 1){
-    return s.toUpperCase();
-  }else if(num === 2){
-    return s.substring(0, randomIndex) + s.charAt(randomIndex).toUpperCase() + s.substring(randomIndex + 1);
-  }else{
-    return s.charAt(0).toUpperCase() + s.slice(1);
-  }
+let lastLetter = (s) => {
+  return s[s.length - 1];
 }
 
-console.log(capitalize('sentence' , 2));
+console.log(lastLetter('last'));
