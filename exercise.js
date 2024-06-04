620ee025a380c93bc4d114cd3f270777f34ffc48
bentlym <metayerbently@gmail.com>
2024-06-03 22:16:14 -0400

    object practice

--- exercise.js
+++ exercise.js
@@ -1,5 +1,25 @@
-let lastLetter = (s) => {
-  return s[s.length - 1];
+class Person {
+  constructor(name,age,country){
+    this._name = name;
+    this._age = age;
+    this._country = country;
+  }
+
+  get name(){
+    return this._name;
+  }
+
+  get age(){
+    return this._age;
+  }
+
+  get country(){
+    return this._country;
+  }
 }
 
-console.log(lastLetter('last'));
+const recruit = new Person('Todd', 20, 'mexico');
+
+for(let props in recruit){
+  console.log(recruit[props]);
+}
