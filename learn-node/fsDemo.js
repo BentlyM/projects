754cb15f38bc3a4c54f0dd944aa48d782fddf099
BentlyM <metayerbently@gmail.com>
2024-08-08 11:47:35 -0400

    reading folder data

---
+++ learn-node/fsDemo.js
@@ -0,0 +1,24 @@
+//import fs from 'fs'
+//import fs from 'fs/promises'
+
+// fs.readFileSync('./learn-node/text.txt', 'utf8', (err , data) => {
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+// const data = fs.readFileSync('./learn-node/text.txt', 'utf-8');
+// console.log(data);
+
+// import fs from 'fs/promises'
+// fs.readFile('./learn-node/text.txt', 'utf-8')
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+
+// const readFile = (async () => {
+//     try{
+//         const data = await fs.readFile('./learn-node/text.txt', 'utf-8');
+//         console.log(data);
+//     }catch(error){
+//         console.log(error);
+//     }
+// })();
