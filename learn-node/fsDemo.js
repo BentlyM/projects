d77c23ead18e55e17affe71fa6a5cbbdc4b20e97
BentlyM <metayerbently@gmail.com>
2024-08-08 11:53:41 -0400

    "Updated fsDemo.js to use fs/promises and added async functions for reading, writing, and appending to a file. Modified text.txt file content."

--- learn-node/fsDemo.js
+++ learn-node/fsDemo.js
@@ -1,5 +1,5 @@
 //import fs from 'fs'
-//import fs from 'fs/promises'
+import fs from 'fs/promises'
 
 // fs.readFileSync('./learn-node/text.txt', 'utf8', (err , data) => {
 //     if(err) throw err;
@@ -14,11 +14,31 @@
 //     .then((data) => console.log(data))
 //     .catch((err) => console.log(err));
 
-// const readFile = (async () => {
-//     try{
-//         const data = await fs.readFile('./learn-node/text.txt', 'utf-8');
-//         console.log(data);
-//     }catch(error){
-//         console.log(error);
-//     }
-// })();
+const readFile = (async () => {
+    try{
+        const data = await fs.readFile('./learn-node/text.txt', 'utf-8');
+        console.log(data);
+    }catch(error){
+        console.log(error);
+    }
+})();
+
+// writeFile()
+
+const writeFile = (async () => {
+    try{
+        await fs.writeFile('./learn-node/text.txt', 'Hello, i am writing to this file');
+        console.log('File written too...');
+    }catch(err){
+        console.log(err);
+    }
+})();
+
+const appendFile = (async ()=>{
+    try{
+        await fs.appendFile('./learn-node/text.txt', '\nThis is append text');
+        console.log('File appended to...');
+    }catch(error){
+        console.log(error);
+    }
+})();
