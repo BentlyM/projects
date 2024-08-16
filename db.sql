10c7313bb48051b9fb97ccc944fe5e78d68862e4
BentlyM <metayerbently@gmail.com>
2024-08-16 15:30:42 -0400

    i will be the world

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE something (some type of data)
+
+-- something being the table name
+
+SELECT * FROM something -- variables can be stacked , multiple arguments can included (destructuring)
+
+SELECT * FROM something ORDER BY condition
+
+SELECT * FROM something WHERE distinction 
+
+DELETE FROM something WHERE condition
+
+SELECT SUM(distinction) FROM something 
